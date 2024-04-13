538110f70bf9df580f7af72226236d8652cceb08
Victor Conner <victor@conner.dev>
2024-04-13 20:44:29 +0200

    chore: Use generic min and max

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -56,20 +56,6 @@ func (m *DB) Connect() func() {
 	}
 }
 
-func min(a, b int64) int64 {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int64) int64 {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 func createDateFilter(minDate, maxDate int64) primitive.D {
 	return bson.D{
 		{
